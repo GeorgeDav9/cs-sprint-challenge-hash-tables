c359d59b37613b6909a0e8d1c0697dd70a1f68ef
George Davila <george.davilagaleas@gmail.com>
2020-08-07 12:52:59 -0400

    ex3 not passing test

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,21 @@ def intersection(arrays):
     """
     # Your code here
 
+    # setup hash table
+    integer_list = {}
+
+    # setup  list
+    result = []
+
+    # single list within hash table
+    for single_list in arrays:
+        for integer in single_list:
+            if integer != integer_list:
+                integer_list[integer] = 1
+            else:
+                # add integer to the intersection
+                result.append(integer)
+
     return result
 
 
